acaf2fafa7fb0eff321231b94fd7895eba1a6831
Duke2412 <66355961+Duke2412@users.noreply.github.com>
2020-10-13 16:28:23 +0700

    Chốt xong bài

---
+++ src/main/java/com/example/demo/repository/CityRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.City;
+import com.example.demo.model.Country;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface CityRepository extends PagingAndSortingRepository<City, Long> {
+    Iterable<City> findAllByCountry(Country country);
+
+    Iterable<City> findAllByName(String name);
+}
